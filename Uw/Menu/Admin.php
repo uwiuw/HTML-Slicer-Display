ab068bc09f2bb28acb114c5b134652908e8e024d
uwiuw3.1.3 <uwiuw@yahoo.com.sg>
2011-12-01 02:21:15 +0700

    support 2 button ajax on one backend page
    Signed-off-by: uwiuw3.1.3 <uwiuw@yahoo.com.sg>

--- Uw/Menu/Admin.php
+++ Uw/Menu/Admin.php
@@ -10,6 +10,7 @@ class Uw_Menu_Admin {
     private $navigation;
     private $curPageSlug;
     private $curPageFile;
+    private $curPageAjCls;
 
     function __construct(Uw_Config_Data $data, Uw_Module_HtmlCreator $html,
         Uw_Menu_Creator $creator) {
@@ -42,10 +43,38 @@ class Uw_Menu_Admin {
             }
         } else {
             if (false !== $clsname = $this->_ifExist($this->curPageFile)) {
-                add_action('admin_print_footer_scripts', array($clsname, 'inject'), 99999); //register button ajax
+                $this->curPageAjCls = $clsname;
             }
             add_Action('admin_menu', array($this, 'regItemMenu')); //register theme menu into backend
         }
+
+    }
+
+    /**
+     * Register Menu items
+     */
+    public function regItemMenu() {
+        $number = 4;
+        add_menu_page('Slicer Syndicate', 'Slicer', 10, 'slicer', array($this, 'loadItemMenu'), '', $number);
+        foreach ($this->navigation as $k => $v) {
+            $name = ucfirst($v);
+            add_submenu_page('slicer', $name, $name, 10, $v, array($this, 'loadItemMenu'), '', $number++);
+        }
+
+    }
+
+    public function loadItemMenu() {
+        $buttons = array();
+        if ($this->curPageAjCls instanceof Uw_Menu_Ajax_Abstract) {
+            add_action('admin_print_footer_scripts', array($this->curPageAjCls, 'inject'), 99999); //register button ajax
+            $buttons = $this->curPageAjCls->getButtons();
+        }
+
+        $clsname = $this->menuItemCls . $this->curPageFile;
+        $clsname = new $clsname($this->config, $this->html, $buttons);
+        $clsname->setNav($this->curPageSlug, $this->curPageFile, $this->navigation);
+        $clsname->init();
+        $this->creator->buildForm($clsname);
 
     }
 
@@ -55,29 +84,8 @@ class Uw_Menu_Admin {
             $clsname = $this->menuAjaxCls . $checkMe;
             return new $clsname($this->config, $this->html);
         }
-        
+
         return false;
-    }
-
-    /**
-     * Register Menu items
-     */
-    public function regItemMenu() {
-        $number = 4;
-        add_menu_page('Slicer Syndicate', 'Slicer', 10, 'slicer', array($this, 'loadItem'), '', $number);
-        foreach ($this->navigation as $k => $v) {
-            $name = ucfirst($v);
-            add_submenu_page('slicer', $name, $name, 10, $v, array($this, 'loadItem'), '', $number++);
-        }
-
-    }
-
-    public function loadItem() {
-        $clsname = $this->menuItemCls . $this->curPageFile;
-        $clsname = new $clsname($this->config, $this->html);
-        $clsname->setNav($this->curPageSlug, $this->curPageFile, $this->navigation);
-        $clsname->init();
-        $this->creator->buildForm($clsname);
 
     }
 
--- Uw/Menu/Ajax/Emergency.php
+++ Uw/Menu/Ajax/Emergency.php
@@ -4,17 +4,41 @@ class Uw_Menu_Ajax_Emergency extends Uw_Menu_Ajax_Abstract {
 
     protected $config;
     protected $itemArgs = array(
-        'form_id' => 'fix_htaccess',
-        'button_id' => 'fix_htaccess_url',
-        'button_id_output' => 'fix_htaccess_url_output',
+        'fix_htaccess' => array(
+            'Name' => 'fix_htaccess',
+            'Title' => 'Fixing htaccess',
+            'Description' => 'reconfigurate theme rewrite rule (will not change blog permalink)',
+            'Ajax' => 'fix_htaccess',
+            'Icon' => 'semlabs_terminal.png',
+            'form_id' => 'fix_htaccess',
+            'button_id' => 'fix_htaccess_url',
+            'button_id_output' => 'fix_htaccess_url_output'
+        ),
+        'test' => array(
+            'Name' => 'test',
+            'Title' => 'Example Button',
+            'Description' => 'Example button of mine ooooh yeah',
+            'Ajax' => 'test',
+            'Icon' => 'semlabs_page_about.png',
+            'form_id' => 'test',
+            'button_id' => 'test_url',
+            'button_id_output' => 'test_url_output'
+        ),
     );
 
     function inject() {
-        $o = $this->html->getButtonAjax(
-            $this->itemArgs['button_id'], $this->itemArgs['form_id'], $this->itemArgs['button_id_output']
-        );
+        foreach ($this->itemArgs as $k => $v) {
+            $o .= $this->html->getButtonAjax(
+                $v['button_id'], $v['form_id'], $v['button_id_output']
+            );
+        }
         $o = '<script type="text/javascript">' . $o . '</script>';
         echo $o;
+
+    }
+
+    function getButtons() {
+        return $this->itemArgs;
 
     }
 
