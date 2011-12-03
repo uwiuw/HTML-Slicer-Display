c808bdca3501f89a5b12b42e004b3dc948898aee
uwiuw3.1.3 <uwiuw@yahoo.com.sg>
2011-12-03 15:03:17 +0700

    adding several phpdoc

--- README.md
+++ README.md
@@ -19,11 +19,3 @@ Windows Xp
 <h3>FEATURE</h3>
 1. Manajement portofolio - portofolio should be able to delete, manage, etc
 2. Upload portfolio menu - the html must be arhieve inside zip file before uploading.
-
-
-
-<h3>TODO</h3>
-1. Everypage can register 2 or more ajax action
-2. ajax delete buttons
-3. Widget for navigation in the front-end
-4. 
--- Uw/Config/Data.php
+++ Uw/Config/Data.php
@@ -1,5 +1,32 @@
 <?php
 
+/**
+ * Uw Framework
+ *
+ * PHP version 5
+ *
+ * @category  Uw
+ * @package   Uw_Config
+ * @author    Aulia Ashari <uwiuw.inlove@gmail.com>
+ * @copyright 2011 Outerim Aulia Ashari
+ * @license   http://dummylicense/ dummylicense License
+ * @version   $SVN: $
+ * @link      http://uwiuw.com/outerrim/
+ */
+
+/**
+ * Uw_Config_Data
+ *
+ * @category   Uw
+ * @package    Uw_Config
+ * @subpackage Uw_Config_Data
+ * @author     Aulia Ashari <uwiuw.inlove@gmail.com>
+ * @copyright  2011 Outerim Aulia Ashari
+ * @license    http://dummylicense/ dummylicense License
+ * @version    Release: @package_version@
+ * @link       http://uwiuw.com/outerrim/
+ * @since      3.0.3
+ */
 class Uw_Config_Data {
 
     private $defaulttheme;
@@ -14,23 +41,23 @@ class Uw_Config_Data {
 
     }
 
-    function get($property) {
-        return $this->$property;
+    function get($prope) {
+        return $this->$prope;
 
     }
 
-    function set($property, $value) {
-        return $this->$property = $value;
+    function set($prope, $value) {
+        return $this->$prope = $value;
 
     }
 
-    function sets(array $properties) {
+    function sets(array $prope) {
 
-        foreach ($properties as $k => $v) {
+        foreach ($prope as $k => $v) {
             $this->$k = $v;
         }
 
-        return $properties;
+        return $prope;
 
     }
 
