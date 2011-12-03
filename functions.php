f19821dacd4e5fb61001537dc40c045fa2531e8d
uwiuw3.1.3 <uwiuw@yahoo.com.sg>
2011-12-03 15:02:07 +0700

    hide the todo file file delete htmcreator and cnage its works only as wrapper for Uw\thirdparty\atTemplate.php from atrim

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ define('UW_U', dirname(WP_CONTENT_URL));
 define('UW_URL', WP_CONTENT_URL . '/themes/' . UW_NAME);
 define('UW_PATH', TEMPLATEPATH);
 
-include_once(UW_PATH . SEP . 'Uw' . SEP . 'Helper' . SEP . 'autoload.php');
+include_once(UW_PATH . SEP . 'Uw' . SEP . 'Autoload.php');
 
 $UwStart = new Uw_Module_Start;
 $config = new Uw_Config_Data;
@@ -15,7 +15,7 @@ $config = $UwStart->init($config, new Uw_Config_Read, get_option(UW_NAME));
 if (is_a($config, 'Uw_Config_Data')) {
     if (is_admin() && is_user_logged_in()) {
         $adminMenuClass = $config->get('admin_menu');
-        $html = new Uw_Module_HtmlCreator;
+        $html = new Uw_Module_Templaty();
         $UwMenu = new $adminMenuClass($config, $html, new Uw_Menu_Creator); //default cls Uw_Menu_Admin
         $UwMenu->init($config);
     } else {
