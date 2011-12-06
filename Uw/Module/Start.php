44ad0635dc9977720c0e28fedf9d0098a562c0bf
uwiuw3.1.3 <uwiuw@yahoo.com.sg>
2011-12-06 18:33:02 +0700

    fixing http://wp.uwiuw.com/issue-compability-theme-option-version-0-0-1-ke-0-0-2/
    Signed-off-by: uwiuw3.1.3 <uwiuw@yahoo.com.sg>

--- Uw/Module/Start.php
+++ Uw/Module/Start.php
@@ -3,32 +3,37 @@
 class Uw_Module_Start {
 
     /**
-     * First time flag
-     * @var bool
-     */
-    private $isFirstTime = true;
-
-    /**
      * Current theme opt
      * @var array
      */
     private $opt = array();
+
+    /**
+     * Config File name (without absolute path)
+     * @var string
+     */
     private $filename = 'firsttime.ini';
 
     public function init(Uw_Config_Data $config, Uw_Config_Read $reader, $opt,
         $filename =''
     ) {
+
+        if ($filename) {
+            $this->filename = $filename;
+        }
+
+        //if $opt not empty, then it's not the first time
         if ($opt) {
-            $this->isFirstTime = false;
-        } else {
-            $this->isFirstTime = true;
-            $htaccess = new Uw_Module_Htaccess();
-            if ($htaccess->setHtaccessFile()) {
-                if ($filename) {
-                    $this->filename = $filename;
-                }
+            /**
+             * see http://wp.uwiuw.com/issue-compability-theme-option-version-0-0-1-ke-0-0-2/
+             */
+            $inInifile = $reader->getOutput($this->filename);
+            if (TRUE === $this->_isNeedToUpgrade($opt, $inInifile)) {
+                $opt = $reader->saveConfig($inInifile);
             }
- 
+        } else {
+            //first time 
+            $this->_rebuildHtaccess();
             $opt = $reader->getOutput($this->filename);
             $opt = $reader->saveConfig($opt); //saving current config into option
         }
@@ -38,6 +43,22 @@ class Uw_Module_Start {
         }
         return $config;
 
+    }
+
+    private function _rebuildHtaccess() {
+        $htaccess = new Uw_Module_Htaccess();
+        return $htaccess->setHtaccessFile();
+
+    }
+
+    private function _isNeedToUpgrade($inDb, $inIniFile) {
+        $mandatory = $inIniFile['mandatory'];
+        
+        $hasildebug = print_r($mandatory, TRUE);
+        echo "\n" . '<pre style="font-size:14px"><hr>' . '$hasildebug ' . htmlentities2($hasildebug) . '</pre>';
+        
+        
+        return False;
     }
 
 }
