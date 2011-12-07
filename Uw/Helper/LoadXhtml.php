9c4bc1127d7533007118881ca9d9890e92003bac
uwiuw3.1.3 <uwiuw@yahoo.com.sg>
2011-12-08 05:44:40 +0700

    adding a better frontend navigation arrow
    Signed-off-by: uwiuw3.1.3 <uwiuw@yahoo.com.sg>

--- Uw/Helper/LoadXhtml.php
+++ Uw/Helper/LoadXhtml.php
@@ -42,15 +42,42 @@ class Uw_Helper_LoadXhtml {
         $nextname = basename($nextFile);
 
         $nav = <<<HTML
-<div style="position:fixed; top:20%; right:2%;">
-    $prevname
-    <br/>
-    <a href ="$prevFile"><img src="$UW_URL/assets/semlabs_arrow_right.png" style="border:none"></a>
-</div>
-<div style="position:fixed; top:20%; left:2%;">
-    $nextname
-    <br/>
-    <a href ="$nextFile"><img src="$UW_URL/assets/semlabs_arrow_left.png" style="border:none"></a>
+<script type='text/javascript' src='/wp-includes/js/jquery/jquery.js'></script>
+<style type="text/css">
+    .navigation {width: 98%; height: 0; margin: 0; padding: 0; position: absolute; top: 0; left: 0;background-color:#fff }
+    .navigation.bottom { top: 100%; margin-top: -108px; clear: both; }
+    .navigation .prev { float: left; text-indent: -9999px; margin: 100px 0 0; width: 40px; height: 40px; clear: none; }
+    .navigation .next { float: right; text-indent: -9999px; margin: 100px 0 0; width: 40px; height: 40px; text-align: left; clear: none; }
+    .navigation .prev a,
+    .navigation .next a { display: block; width: 40px; height: 40px; }
+    .navigation .prev a { background: url($UW_URL/assets/semlabs_arrow_left.png) no-repeat left top; }
+    .navigation .next a { background: url($UW_URL/assets/semlabs_arrow_right.png) no-repeat right top; }
+    .navigation .prev a:hover { background: url($UW_URL/assets/semlabs_arrow_left.png) no-repeat left bottom; }
+    .navigation .next a:hover { background: url($UW_URL/assets/semlabs_arrow_right.png) no-repeat right bottom; }
+</style>
+<script>
+    // once the dom is loaded
+    jQuery(document).ready(function(){
+        if ( jQuery(".navigation").length ) {
+            jQuery(".navigation.bottom").remove();
+            var navi = jQuery(".navigation"),
+            scrollTop = jQuery(window).scrollTop(),
+            lowerBound = navi.parent().height() - 108;
+            jQuery(window).scroll(function(){
+                scrollTop = jQuery(window).scrollTop();
+                lowerBound = navi.parent().height() - 108;
+                if ( parseInt(navi.css("top")) <= lowerBound && scrollTop <= lowerBound ) {
+                    navi.stop().animate({ top: scrollTop },300);
+                } else {
+                    navi.stop().animate({ top: lowerBound },300);
+                }
+            });
+        }
+    });
+</script>
+<div class="navigation">
+    <div class="prev"><a href ="$prevFile">Prev2</a></div>
+    <div class="next"><a href ="$nextFile">Next</a></div>
 </div>
 </body>
 HTML;
