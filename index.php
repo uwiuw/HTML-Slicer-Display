504f6386d9c0633b13630914e85dfb8556577827
uwiuw3.1.3 <uwiuw@yahoo.com.sg>
2011-12-11 14:59:17 +0700

    adding search forto widget - case insensitive, smart-lookup when searcher only input part of the porto name. adding new css- need to find the right css reset
    Signed-off-by: uwiuw3.1.3 <uwiuw@yahoo.com.sg>

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
+
+if (is_search() && FALSE != $search_query = strtolower(trim(get_search_query()))) {
+
+    foreach ($listofthemes as $k => $v) {
+        if (strtolower($v) === $search_query) {
+            $themename = $search_query;
+            break;
+        } elseif (false !== stristr($v, $search_query)) {
+            $themename = $v;
+            break;
+        }
+    }
+}
+
 $loadXhtml = new Uw_Module_Loader($html, $Uw_Sidebar);
 $loadXhtml->show($themename, 'index.html', $listofthemes);
